c0b9bb560fc1cc8d38a80cce4565cd478e17a2c5
jbis9051 <josh9051@gmail.com>
2020-03-24 17:29:39 -0400

    Fill content

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const useragent = require('express-useragent');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -16,6 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(useragent.express());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about');
+router.get('/about', function (req, res, next) {
+    res.render('about');
 });
 
+router.get(['/download/:platform?'], function (req, res, next) {
+    let platform = "windows";
+
+    if (req.useragent.isMac) {
+        platform = "macos";
+    }
+
+    if (req.useragent.isLinux) {
+        platform = "linux";
+    }
+
+    const names = {
+        'windows': 'Windows',
+        'macos': 'macOS',
+        'linux': 'Linux',
+    };
+
+    if(names[req.params.platform]){
+        platform = req.params.platform;
+    }
+
+    res.render('download', {platform, names});
+
+});
 
 
 module.exports = router;
