ccd36414b332d3887c9bd14afeede0c1d38eeaeb
jbis9051 <josh9051@gmail.com>
2020-10-26 18:56:02 -0400

    Fix links

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "731b9047ea65277d6544";
+/******/ 	var hotCurrentHash = "a8e9e8e38ad16140adbf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
